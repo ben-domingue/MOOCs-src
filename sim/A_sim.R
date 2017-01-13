8080e9d688da7e78d31c43beefac3e9306413560
ben-domingue <ben.domingue@gmail.com>
2017-01-13 15:37:20 -0800

    updated sim code to focus on correlations of disc/diff params

--- sim/A_sim.R
+++ sim/A_sim.R
@@ -6,8 +6,8 @@ ff<-function(M,N=1000,n=50) {
         data.frame(th0=theta0,sl=sl)->th
         out<-list()
         for (i in 1:n) {
-                                        #th$th0+th$sl*i/n+rnorm(nrow(th),mean=0,sd=.03) -> x
-            rnorm(N)->x
+            th$th0+th$sl*i/n+rnorm(nrow(th),mean=0,sd=.03) -> x
+            #rnorm(N)->x #putzing around with totally random theta at each item
             a[i]*(x-b[i])->kern
             exp(kern)->ek
             ek/(1+ek)->pr
@@ -29,39 +29,43 @@ ff<-function(M,N=1000,n=50) {
         coef(mod)->co
         co[-length(co)]->co
         do.call("rbind",co)->co
-        ##
-        a->x
-        co[,1]->y
-        y-x->z
-        1:length(z)->xv
-        lm(z~xv)->mod
-        coef(mod)[2]->m1
-        lm(z~x)->mod
-        coef(mod)[2]->m2
-        ##
-        b->x
-        -1*co[,2]->y
-        y-x->z
-        1:length(z)->xv
-        lm(z~xv)->mod
-        coef(mod)[2]->m3
-        lm(z~x)->mod
-        coef(mod)[2]->m4
-        c(m1,m2,m3,m4)->tr[[i]]
+        #plot(a,co[,1])
+        cor(a,co[,1])->c1
+        cor(b,-1*co[,2])->c2
+        c(c1,c2)->tr[[i]]
+        ## ##
+        ## a->x
+        ## co[,1]->y
+        ## y-x->z
+        ## 1:length(z)->xv
+        ## lm(z~xv)->mod
+        ## coef(mod)[2]->m1
+        ## lm(z~x)->mod
+        ## coef(mod)[2]->m2
+        ## ##
+        ## b->x
+        ## -1*co[,2]->y
+        ## y-x->z
+        ## 1:length(z)->xv
+        ## lm(z~xv)->mod
+        ## coef(mod)[2]->m3
+        ## lm(z~x)->mod
+        ## coef(mod)[2]->m4
+        ## c(m1,m2,m3,m4)->tr[[i]]
     }
     do.call("rbind",tr)->out
     out
 }
 
 out<-list()
-for (M in seq(0,1,by=.25)) ff(M=0)->out[[as.character(M)]]
+for (M in seq(0,2,by=.25)) ff(M=0)->out[[as.character(M)]]
 out->hold
 for (i in 1:length(out)) cbind(as.numeric(names(out)[i]),out[[i]])->out[[i]]
 data.frame(do.call("rbind",out))->tab
 
-names(tab)<-c("M","a.order","a.true","b.order","b.true")
-par(mfrow=c(2,2))
-for (i in 2:5) {
+names(tab)<-c("M","a.cor","a.b.cor")
+par(mfrow=c(2,1))
+for (i in 2:3) {
     boxplot(tab[[names(tab)[i] ]]~tab$M)
     abline(h=0)
 }
